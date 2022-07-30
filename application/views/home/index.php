8fe19a5734e46d97469585d216efb340b6fe3126
naseemm06 <92108669+naseemm06@users.noreply.github.com>
2022-07-30 13:35:58 +0700

    main

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,3 +1,11 @@
 
 <h1>Hello, <?= $nama; ?> !</h1>
 
+<div class="card text-center">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="<?php echo base_url('assets/images/2.jpg'); ?>"/100px180/?text=Image cap" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">NAME: Naseem Umar Masaid</p>
+  </div>
+</div>
+</div>
